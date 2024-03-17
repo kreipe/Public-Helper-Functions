34e96b0e9fe125adee356834892b5874d0b149e8
kreipe <37271598+kreipe@users.noreply.github.com>
2024-03-17 11:11:52 -0700

    Update Helper_Functions_2.py

--- Helper_Functions_2.py
+++ Helper_Functions_2.py
@@ -419,3 +419,26 @@ def calculate_results(y_true, y_pred):
 # 1 model_name = model.evaluate(test_data)
 # 2 load_results_model_name
 
+def mean_absolute_scaled_error(y_true, y_pred):
+    mae = tf.reduce_mean(tf.abs(y_true - y_pred))
+    mae_naive_no_season = tf.reduce_mean(tf.abs(y_true[1:] - y_true[:-1]))
+    return mae / mae_naive_no_season
+
+def evaluate_preds(y_true, y_pred):
+    # make sure using float32 for metrics calcs
+    y_true = tf.cast(y_true, dtype=tf.float32)
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+    # Calc various metrics
+
+    mae = keras.metrics.mean_absolute_error(y_true, y_pred)
+    mse = keras.metrics.mean_squared_error(y_true, y_pred) #emphasises outliers
+    rmse = tf.sqrt(mse)
+    mape = keras.metrics.mean_absolute_percentage_error(y_true, y_pred)
+    mase = mean_absolute_scaled_error(y_true, y_pred)
+
+    return{'mae': mae.numpy(),
+           'mse': mse.numpy(),
+           'rmse': rmse.numpy(),
+           'mape': mape.numpy(),
+           'mase': mase.numpy()}
